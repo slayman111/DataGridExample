e2de88ee048f6623e0d48984ed24aef3291b217a
slayman111 <77606207+slayman111@users.noreply.github.com>
2021-09-14 10:15:28 +0300

    Add files via upload

---
+++ DataGridExample1/DataGridExample1/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DataGridExample1
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsMember { get; set; }
+    }
+}
---
+++ DataGridExample1/DataGridExample1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace DataGridExample1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        { 
+            InitializeComponent();
+
+            Customer cust1 = new Customer()
+            {
+                FirstName = "firstname1",
+                LastName = "lastname1",
+                IsMember = true
+            };
+            Customer cust2 = new Customer()
+            {
+                FirstName = "firstname2",
+                LastName = "lastname2",
+                IsMember = true
+            };
+            Customer cust3 = new Customer()
+            {
+                FirstName = "firstname3",
+                LastName = "lastname3",
+                IsMember = false
+            };
+
+            ObservableCollection<Customer> custdata = new ObservableCollection<Customer>()
+            {
+                cust1,
+                cust2,
+                cust3
+            };
+
+            CustomerGrid.DataContext = custdata;
+        }
+    }
+}
